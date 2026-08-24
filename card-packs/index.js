19a632e8b76391b933c27e7b6bda792266144f01
EpicenterGUY <mintbamtol7@gmail.com>
2026-08-24 13:53:05 +0900

    pack03 지역 연계팩을 카드 레지스트리에 등록

--- card-packs/index.js
+++ card-packs/index.js
@@ -1,11 +1,12 @@
 (function(root,factory){
   const api=factory(
     typeof module!=='undefined'?require('./pack01.js'):root.PACK01_CARDS,
-    typeof module!=='undefined'?require('./pack02.js'):root.PACK02_CARDS
+    typeof module!=='undefined'?require('./pack02.js'):root.PACK02_CARDS,
+    typeof module!=='undefined'?require('./pack03.js'):root.PACK03_CARDS
   );
   if(typeof module!=='undefined')module.exports=api;
   Object.assign(root,api);
-})(typeof globalThis!=='undefined'?globalThis:this,function(PACK01_CARDS,PACK02_CARDS){
+})(typeof globalThis!=='undefined'?globalThis:this,function(PACK01_CARDS,PACK02_CARDS,PACK03_CARDS){
   const makePack=(metadata,cards)=>Object.freeze({
     ...metadata,
     cards:Object.freeze(cards),
@@ -15,7 +16,8 @@
   // Extension point: import a pack module above and add one metadata entry here.
   const CARD_PACK_LIST=Object.freeze([
     makePack({id:'pack01',name:'신규 1팩',version:'1.0.0',enabledByDefault:true,rewardWeight:1},PACK01_CARDS),
-    makePack({id:'pack02',name:'조건부 고점팩',version:'1.0.0',enabledByDefault:true,rewardWeight:1},PACK02_CARDS)
+    makePack({id:'pack02',name:'조건부 고점팩',version:'1.0.0',enabledByDefault:true,rewardWeight:1},PACK02_CARDS),
+    makePack({id:'pack03',name:'지역 연계팩',version:'1.0.0',enabledByDefault:true,rewardWeight:1},PACK03_CARDS)
   ]);
   const CARD_PACKS=Object.freeze(Object.fromEntries(CARD_PACK_LIST.map(pack=>[pack.id,pack])));
 
